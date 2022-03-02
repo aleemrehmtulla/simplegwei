fb0490bffe484044d5dd7a974dbdd1b5f7c49a15
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-03-02 12:51:58 -0500

    buttons responsive

--- pages/index.js
+++ pages/index.js
@@ -92,7 +92,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <div className="flex px-20 justify-end h-full">
+      <div className="flex px-7 md:px-20 justify-end h-full">
         <button className='hover:cursor-pointer pr-6'> 
           <a target="_blank" rel="noreferrer" href='https://github.com/aleemrehmtulla/simplegwei'>
             <BsGithub size={40} className={`${dark ? 'fill-white' : 'fill-black'}`}/>
